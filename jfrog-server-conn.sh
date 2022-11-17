33feb52f7406d5aa75bb84620e58b66297789ec4
NarsiMyTeaching <109086666+NarsiMyTeaching@users.noreply.github.com>
2022-11-17 21:43:14 +0530

    Create jfrog-server-conn.sh
    added the jfrog server connection file

---
+++ jfrog-server-conn.sh
@@ -0,0 +1,29 @@
+server=http://13.235.201.185:8082/artifactory
+repo=libs-snapshot-local
+
+userName=admin
+password=Adance123$
+
+# Maven artifact location
+name=we-shopify-platform
+artifact=com/dsoft/$name/0.0.1-SNAPSHOT
+path=$server/$repo/$artifact
+echo $path
+
+build=$(curl -u "admin":"Adance123$" $path/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+echo $build
+
+extension=$(curl -u "admin":"Adance123$" $path/maven-metadata.xml | grep '<extension>' | head -2 | sed "s/.*<extension>\([^<]*\)<\/extension>.*/\2/")
+echo $extension
+
+artifact_name=$name-$build.war
+echo $artifact_name
+
+url=$path/$artifact_name
+
+# Download
+echo $url
+wget  --user=$userName --password=$password $url
+mkdir app-files
+chmod 777 app-files
+cp $artifact_name app-files/$name.war
